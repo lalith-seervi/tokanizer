a4e183a32edb07d74e1315a2fbd7627afaf4197f
lalith-seervi <154885188+lalith-seervi@users.noreply.github.com>
2025-11-16 21:10:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,10 +30,10 @@
 
 
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World from Flask on Vercel!"
+    return render_template("index.html")
